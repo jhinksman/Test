dc0d635f8e454944190845a7750ca86a5b80c376
jhonh <jhonh@DESKTOP-EM019J0.attlocal.net>
2021-06-16 16:03:11 -0400

    Added a for loop

--- gitHubCommitTest/src/gitHubCommitTest/TestCommit.java
+++ gitHubCommitTest/src/gitHubCommitTest/TestCommit.java
@@ -3,8 +3,9 @@ package gitHubCommitTest;
 public class TestCommit {
 
 	public static void main(String[] args) {
- 
+		for(int i=0;i<3;i++) {
 		System.out.println("This is an attempt to commit a project to GitHub.");
+		}
 	}
 
 }
